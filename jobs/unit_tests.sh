27f7c8fa49ce685ab565ec197b87688313015d0f
Ivan Petuhov <satels@gmail.com>
2017-04-20 23:04:18 +0300

    continue - add docker compose files and other util files

---
+++ jobs/unit_tests.sh
@@ -0,0 +1,1 @@
+coverage run --source='.' ./myshop/manage.py test ./myshop --keepdb --traceback --verbosity=3 --noinput && coverage report
---
+++ myshop/sms/tests/test_handlers.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class MainTest(unittest.TestCase):
+
+    maxDiff = None
+
+    def test_dummy(self):
+        pass
+
+    def test_smscru(self):
+        pass
+
+    def test_update_conf(self):
+        pass
+
+    def test_get_conf(self):
+        pass
